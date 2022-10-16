3a34523a08cf32c079f629d201bc8455957e5e08
barizahaqi <haqibariza@gmail.com>
2022-10-16 13:14:42 +0700

    update terakhir fix

--- src/AlphaBetaBot.py
+++ src/AlphaBetaBot.py
@@ -2,53 +2,48 @@ from Bot import Bot
 from AlphaBetaNode import AlphaBetaNode
 from GameAction import GameAction
 from GameState import GameState
-import copy
+
 
 class AlphaBetaBot(Bot):
-    def alphaBetaAlgorithm(self, node, playerTurn, isMax, depth, move, alpha, beta):
-        #algoritma alpha beta pruning
-        if len(node.listMove) == 0 or depth == 0:
-            return [move, node.skor[playerTurn] - node.skor[not playerTurn]] # base case
-        else:
-            if isMax:
-                bestScore = -10
-                bestMove =  ()
-                for x, y in node.listMove:
-                    currentNode = node.copy() #buat node baru untuk ditelusuri
-                    turn = currentNode.move(playerTurn, x, y)
-                    result = self.alphaBetaAlgorithm(currentNode, playerTurn, turn, depth - 1, (x, y), alpha, beta)
-                    if result[1] > bestScore:
-                        bestScore = result[1]
-                        bestMove = (x, y)
-                    alpha = max(bestScore, alpha)
-                    if beta <= alpha:
-                        break
-                return [bestMove, bestScore]
-            else:
-                worstScore = 10
-                worstMove = ()
-                for x, y in node.listMove:
-                    currentNode = node.copy() 
-                    turn = currentNode.move(not playerTurn, x, y)
-                    result = self.alphaBetaAlgorithm(currentNode, playerTurn, not turn, depth - 1, (x, y), alpha, beta)
-                    if result[1] < worstScore:
-                        worstScore = result[1]
-                        worstMove = (x, y)
-                    beta = min(beta, worstScore)
-                    if beta <= alpha:
-                        break
-                return [worstMove, worstScore]
-
     def get_action(self, state: GameState) -> GameAction:
-        #GameAction yang akan dikirimkan ke permainan
-        row_status = state.row_status
-        col_status = state.col_status
-        node=AlphaBetaNode()
-        node.update(row_status, col_status)
-        moveResult = self.alphaBetaAlgorithm(node,True,True, 6,(0,0),-10,10)[0]
-        if moveResult[0]%2==0:
+        # GameAction yang akan dikirimkan ke permainan
+        node = AlphaBetaNode()
+        node.update(state.row_status, state.col_status)
+        moveResult = self.alphaBetaAlgorithm(node, True, 7, (0, 0), -10, 10)[0]
+        if moveResult[0] % 2 == 0:
             return GameAction('row', [moveResult[1], moveResult[0]//2])
         else:
             return GameAction('col', [moveResult[1], moveResult[0]//2])
 
-    
+    def alphaBetaAlgorithm(self, node, playerTurn, depth, move, alpha, beta):
+        # algoritma alpha beta pruning
+        if depth == 0 or len(node.listMove) == 0:
+            return [move, node.score[True] - node.score[False]]
+        else:
+            move = ()
+            if playerTurn:  # jika true akan mencari nilai maksimum
+                bestScore = -9
+                for x, y in node.listMove:
+                    currentNode = node.copy()  # buat node baru untuk ditelusuri
+                    turn = currentNode.move(playerTurn, x, y)
+                    result = self.alphaBetaAlgorithm(currentNode, turn, depth - 1, (x, y), alpha, beta)
+                    if result[1] > bestScore:
+                        bestScore = result[1]
+                        move = (x, y)
+                    alpha = max(alpha,bestScore)
+                    if beta <= alpha:
+                        break
+                return [move, bestScore]
+            else: # false akan mencari nilai minimum
+                worstScore = 9
+                for x, y in node.listMove:
+                    currentNode = node.copy()
+                    turn = currentNode.move(playerTurn, x, y)
+                    result = self.alphaBetaAlgorithm(currentNode, not turn, depth - 1, (x, y), alpha, beta)
+                    if result[1] < worstScore:
+                        worstScore = result[1]
+                        move = (x, y)
+                    beta = min(beta, worstScore)
+                    if beta <= alpha:
+                        break
+                return [move, worstScore]
--- src/AlphaBetaNode.py
+++ src/AlphaBetaNode.py
@@ -1,67 +1,60 @@
 class AlphaBetaNode:
-    #class untuk membuat game Dots and Boxes yang dimainkan secara otomatis untuk pohon algoritma
+    # class untuk node pohon alpha beta pruning
     def __init__(self):
-        self.board = [None] * 24 # total 24 buah garis
-        self.listMove = [] #move yang tersedia
-        self.skor = [0, 0] # skor yang didapat dari banyaknya kotak yang dibuat. skor[0] = lawan, skor[1] = playerTurn
+        self.board = [None] * 24  # total 24 buah garis
+        self.listMove = []  # move yang tersedia
+        self.score = [0, 0] # score yang didapat dari banyaknya kotak yang dibuat. score[0] = lawan, score[1] = playerTurn
 
     def update(self, row, col):
-        #menyesuaikan variabel listMove dan board dengan list move yang tersedia pada game yang sedang berlangsung
-        j=0
+        # menyesuaikan variabel listMove dan board dengan list move yang tersedia pada game yang sedang berlangsung
+        j = 0
         for i in range(7):
-            if i%2==0: #horizontal
-                while (j<3):
+            if i % 2 == 0:  # horizontal
+                while (j < 3):
                     if (row[i//2][j] == 1):
                         self.board[i*4-i//2 + j] = True
                     else:
                         self.listMove.append((i, j))
-                    j+=1
-            else: #vertical
-                while (j<4):
+                    j += 1
+            else:  # vertical
+                while (j < 4):
                     if (col[i//2][j] == 1):
-                        self.board[i*4 -(i-1)//2 + j - 1] = True
+                        self.board[i*4 - (i-1)//2 + j - 1] = True
                     else:
                         self.listMove.append((i, j))
-                    j+=1
-            j=0
-    
-    def formedBox(self, playerTurn, x, y): 
-        #menambah poin dan mengembalikan true jika ada kotak yang terbentuk
-        formed = False
-        if x % 2 == 0: # horizontal
-            if x<6 and self.board[x*4-x//2 + y] and self.board[x*4-x//2 + y+3] and self.board[x*4-x//2 + y + 4] and self.board[(x+2)*4-x//2+ y -1]:
-                #kotak bagian atas
-                self.skor[playerTurn] += 1
-                formed = True
-            if x>0 and self.board[x*4-x//2 + y] and self.board[x*4-x//2 + y-3] and self.board[x*4-x//2 + y - 4] and self.board[(x-2)*4-x//2+ y +1]:
-                #kotak bagian bawah
-                self.skor[playerTurn] += 1
-                formed = True
-        else: #x%2==1, vertical
-            if y<3 and self.board[x*4 -(x-1)//2 + y -1] and self.board[x*4-(x-1)//2 + y] and self.board[x*4-(x-1)//2 + y - 4] and self.board[(x+1)*4-(x-1)//2+ y -1]:
-                #kotak bagian kanan
-                self.skor[playerTurn] += 1
-                formed = True
-            if y>0 and self.board[x*4 -(x-1)//2 + y -2] and self.board[x*4-(x-1)//2 + y-1] and self.board[x*4-(x-1)//2 + y - 5] and self.board[(x+1)*4-(x-1)//2+ y -2]:
-                #kotak bagian kiri
-                self.skor[playerTurn] += 1
-                formed = True
-        return formed
+                    j += 1
+            j = 0
 
     def move(self, playerTurn, x, y):
-        #mengisi board dan menghapus move pada listMove
-        if x % 2 == 0:
+        # menambah poin dan mengembalikan true jika ada kotak yang terbentuk
+        formed = False
+        if x % 2 == 0:  # horizontal
             self.board[x*4-x//2 + y] = True
-        else:
-            self.board[x*4-(x-1)//2 + y -1] = True
+            if x < 6 and self.board[x*4-x//2 + y] and self.board[x*4-x//2 + y+3] and self.board[x*4-x//2 + y + 4] and self.board[(x+2)*4-x//2 + y - 1]:
+                # kotak bagian atas
+                self.score[playerTurn] += 1
+                formed = True
+            if x > 0 and self.board[x*4-x//2 + y] and self.board[x*4-x//2 + y-3] and self.board[x*4-x//2 + y - 4] and self.board[(x-2)*4-x//2 + y + 1]:
+                # kotak bagian bawah
+                self.score[playerTurn] += 1
+                formed = True
+        else:  # x%2==1, vertical
+            self.board[x*4-(x-1)//2 + y - 1] = True
+            if y < 3 and self.board[x*4 - (x-1)//2 + y - 1] and self.board[x*4-(x-1)//2 + y] and self.board[x*4-(x-1)//2 + y - 4] and self.board[(x+1)*4-(x-1)//2 + y - 1]:
+                # kotak bagian kanan
+                self.score[playerTurn] += 1
+                formed = True
+            if y > 0 and self.board[x*4 - (x-1)//2 + y - 2] and self.board[x*4-(x-1)//2 + y-1] and self.board[x*4-(x-1)//2 + y - 5] and self.board[(x+1)*4-(x-1)//2 + y - 2]:
+                # kotak bagian kiri
+                self.score[playerTurn] += 1
+                formed = True
         self.listMove.remove((x, y))
-        turn = self.formedBox(playerTurn, x, y) #jika true, playerTurn akan mendapatkan gilirannya lagi
-        return turn
+        return formed
 
     def copy(self):
-        #membuat copy dari objek sendiri
+        # membuat copy dari objek sendiri
         newNode = AlphaBetaNode()
         newNode.board = self.board[:]
         newNode.listMove = self.listMove[:]
-        newNode.skor = self.skor[:]
+        newNode.score = self.score[:]
         return newNode
