5718484189798de32f80afa975d5bb26154ebb89
dzaky.fattan <70198829+DzakyFattan@users.noreply.github.com>
2022-10-17 11:05:03 +0700

    add desc on AlphaBetaBot(), very little refactor

--- src/AlphaBetaBot.py
+++ src/AlphaBetaBot.py
@@ -7,22 +7,31 @@ import numpy as np
 
 class AlphaBetaBot(Bot):
     def get_action(self, state: GameState) -> GameAction:
+        """
+        menentukan aksi yang akan diambil bot
+        """
         node = AlphaBetaNode()
         node.update(state.row_status, state.col_status)
-        countRowAndColNotMarked = 24 - np.count_nonzero(state.row_status == 1) - np.count_nonzero(state.col_status == 1)
+        countRowAndColNotMarked = 24 - \
+            np.count_nonzero(state.row_status == 1) - \
+            np.count_nonzero(state.col_status == 1)
         maxNode, depth = 1, 0
-        while (maxNode<4037880 and countRowAndColNotMarked>0): # batasi maxNode yang dibuat sampai 4037880
-            maxNode *=countRowAndColNotMarked
+        # batasi maxNode yang dibuat sampai 4037880
+        while (maxNode < 4037880 and countRowAndColNotMarked > 0):
+            maxNode *= countRowAndColNotMarked
             depth += 1
             countRowAndColNotMarked -= 1
-        moveResult = self.alphaBetaAlgorithm(node, True, depth, (0, 0), -10, 10)[0]
+        moveResult = self.alphaBetaAlgorithm(
+            node, True, depth, (0, 0), -10, 10)[0]
         if moveResult[0] % 2 == 0:
             return GameAction('row', [moveResult[1], moveResult[0]//2])
         else:
             return GameAction('col', [moveResult[1], moveResult[0]//2])
 
     def alphaBetaAlgorithm(self, node, playerTurn, depth, move, alpha, beta):
-        # algoritma alpha beta pruning
+        """
+        algoritma alpha beta pruning
+        """
         if depth == 0 or len(node.listMove) == 0:
             return [move, node.score[True] - node.score[False]]
         else:
@@ -32,20 +41,22 @@ class AlphaBetaBot(Bot):
                 for x, y in node.listMove:
                     currentNode = node.copy()  # buat node baru untuk ditelusuri
                     turn = currentNode.move(playerTurn, x, y)
-                    result = self.alphaBetaAlgorithm(currentNode, turn, depth - 1, (x, y), alpha, beta)
+                    result = self.alphaBetaAlgorithm(
+                        currentNode, turn, depth - 1, (x, y), alpha, beta)
                     if result[1] > bestScore:
                         bestScore = result[1]
                         move = (x, y)
-                    alpha = max(alpha,bestScore)
+                    alpha = max(alpha, bestScore)
                     if beta <= alpha:
                         break
                 return [move, bestScore]
-            else: # false akan mencari nilai minimum
+            else:  # false akan mencari nilai minimum
                 worstScore = 9
                 for x, y in node.listMove:
                     currentNode = node.copy()
                     turn = currentNode.move(playerTurn, x, y)
-                    result = self.alphaBetaAlgorithm(currentNode, not turn, depth - 1, (x, y), alpha, beta)
+                    result = self.alphaBetaAlgorithm(
+                        currentNode, not turn, depth - 1, (x, y), alpha, beta)
                     if result[1] < worstScore:
                         worstScore = result[1]
                         move = (x, y)
--- src/AlphaBetaNode.py
+++ src/AlphaBetaNode.py
@@ -2,11 +2,15 @@ import random
 
 
 class AlphaBetaNode:
-    # class untuk node pohon alpha beta pruning
+    """
+    class untuk node pohon Alpha Beta Pruning
+    """
+
     def __init__(self):
         self.board = [None] * 24  # total 24 buah garis
         self.listMove = []  # move yang tersedia
-        self.score = [0, 0] # score yang didapat dari banyaknya kotak yang dibuat. score[0] = lawan, score[1] = playerTurn
+        # score yang didapat dari banyaknya kotak yang dibuat. score[0] = lawan, score[1] = playerTurn
+        self.score = [0, 0]
 
     def update(self, row, col):
         # menyesuaikan variabel listMove dan board dengan list move yang tersedia pada game yang sedang berlangsung
@@ -30,33 +34,37 @@ class AlphaBetaNode:
         random.shuffle(self.listMove)
 
     def move(self, playerTurn, x, y):
-        # menambah poin dan mengembalikan true jika ada kotak yang terbentuk
-        formed = False
+        """
+        menambah poin dan mengembalikan true jika ada kotak yang terbentuk
+        """
+        self.listMove.remove((x, y))
         if x % 2 == 0:  # horizontal
             self.board[x*4-x//2 + y] = True
             if x < 6 and self.board[x*4-x//2 + y] and self.board[x*4-x//2 + y+3] and self.board[x*4-x//2 + y + 4] and self.board[(x+2)*4-x//2 + y - 1]:
                 # kotak bagian atas
                 self.score[playerTurn] += 1
-                formed = True
+                return True
             if x > 0 and self.board[x*4-x//2 + y] and self.board[x*4-x//2 + y-3] and self.board[x*4-x//2 + y - 4] and self.board[(x-2)*4-x//2 + y + 1]:
                 # kotak bagian bawah
                 self.score[playerTurn] += 1
-                formed = True
+                return True
         else:  # x%2==1, vertical
             self.board[x*4-(x-1)//2 + y - 1] = True
             if y < 3 and self.board[x*4 - (x-1)//2 + y - 1] and self.board[x*4-(x-1)//2 + y] and self.board[x*4-(x-1)//2 + y - 4] and self.board[(x+1)*4-(x-1)//2 + y - 1]:
                 # kotak bagian kanan
                 self.score[playerTurn] += 1
-                formed = True
+                return True
             if y > 0 and self.board[x*4 - (x-1)//2 + y - 2] and self.board[x*4-(x-1)//2 + y-1] and self.board[x*4-(x-1)//2 + y - 5] and self.board[(x+1)*4-(x-1)//2 + y - 2]:
                 # kotak bagian kiri
                 self.score[playerTurn] += 1
-                formed = True
-        self.listMove.remove((x, y))
-        return formed
+                return True
+        return False
 
     def copy(self):
-        # membuat copy dari objek sendiri
+        """
+        membuat copy dari objek sendiri
+        """
+
         newNode = AlphaBetaNode()
         newNode.board = self.board[:]
         newNode.listMove = self.listMove[:]
